748fc71731d0c901da6b69d25ccb3e620186c866
charles <ketchasso72@gmail.com>
2023-05-05 22:15:39 +0100

    invite

---
+++ src/Components/Organisms/DataGrid/GridRows.jsx
@@ -0,0 +1,31 @@
+const rows = [
+  {
+    id: 1,
+    task: 'cleanup',
+    selfAssesement: 'good',
+    progress: 25,
+    assignee: 'arrey-etta',
+    dueDate: 'Oct &',
+    seen: 'True',
+  },
+  {
+    id: 2,
+    task: 'code',
+    selfAssesement: 'bad',
+    progress: 65,
+    assignee: 'alexandar',
+    dueDate: 'Jan &',
+    seen: 'false',
+  },
+  {
+    id: 3,
+    task: 'Study',
+    selfAssesement: 'Exellent',
+    progress: 55,
+    assignee: 'Danny Jennifer',
+    dueDate: 'Oct &',
+    seen: 'True',
+  },
+];
+
+export default rows;
