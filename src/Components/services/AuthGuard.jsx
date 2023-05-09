2f6ab710d5836aaed12ceed8fd5bba751c90cee8
Godden13 <chrissgodden@gmail.com>
2023-05-09 16:26:12 +0100

    add higher order component

---
+++ src/Components/services/AuthGuard.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable no-shadow */
+/* eslint-disable react/jsx-props-no-spreading */
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getCurrentUser } from '../../Api/auth';
+
+let currentUser;
+
+export default function AuthGuard(Component) {
+  function Guard(props) {
+    const [user, setUser] = useState(currentUser);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      if (user) return;
+      getCurrentUser()
+        .then((user) => {
+          setUser(user);
+          currentUser = user;
+        })
+        .catch(() => {
+          navigate('/login', { replace: true });
+        });
+    }, [navigate, user]);
+
+    return user ? <Component {...props} user={user} /> : <>loading...</>;
+  }
+
+  return Guard;
+}
